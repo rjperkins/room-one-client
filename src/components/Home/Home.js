57fc1a152a17b7ba7bf66875da327837918af23b
Richard Perkins <richardjperkins89@gmail.com>
2020-06-23 20:19:56 +0200

    fix: further homepage css fixes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,18 +5,10 @@ import './Home.css'
 
 export default function Home () {
   return (
-    <div className="all-home" style={{ minHeight: '100vh', paddingTop: '40px' }}>
-      <div className="cover" style={{
-        backgroundImage: `url(${room1cover})`, backgroundSize: '100%', backgroundRepeat: 'no-repeat', width: 'auto', display: 'flex', justifyContent: 'center', fontFamily: 'Work Sans'
-      }}>
-        {/* <div style={{ textAlign: 'center', fontSize: '3rem', color: 'white', padding: '100px', float: 'inline-end' }}>WELCOME TO THE HOME OF ROOM 1</div> */}
-        <img className='cover-image-home' src={room1cover} alt="room 1 cover" style={{ maxHeight: '750px' }}></img>
-      </div>
+    <div className="all-home" style={{ minHeight: '100vh' }}>
+      <img className='cover-image-home' src={room1cover} alt="room 1 cover" style={{ maxHeight: '100vh', margin: '0 auto' }}></img>
       <div className="info-dan" style={{ display: 'flex', margin: '5px auto', maxWidth: '900px', alignItems: 'center', justifyContent: 'center', color: 'gray', fontSize: '18px', fontWeight: 'bold', textAlign: 'justify', padding: '10px' }}>
         <img src={altitudeHome} alt="dj-altitude" style={{ height: '200px', margin: '20px' }}></img>
-        {/*  Daniel Bradburn
-        <br />
-        <br /> */}
         <p style={{ textAlign: 'center' }}>Made in the Seychelles, raised in London and now resident in Barcelona. Altitude is the founder of Room 1.
         Known for his ability to take you on a flight through liquid and atmospheric Drum and Bass.
         <br />
@@ -24,7 +16,6 @@ export default function Home () {
         <br />
         This is Altitude.
         </p>
-
       </div>
     </div >
   )
