c31d42573f384efb892dd2530654e6ad7bcc5b64
Richard Perkins <richardjperkins89@gmail.com>
2020-09-09 13:36:25 +0200

    refactor: move firebase config variables to env file

--- src/Services/firebase.js
+++ src/Services/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-Uup_L_XFIanIUCWRFcKfJjeSSwMTqBE",
-  authDomain: "room-one.firebaseapp.com",
-  databaseURL: "https://room-one.firebaseio.com",
-  projectId: "room-one",
-  storageBucket: "room-one.appspot.com",
-  messagingSenderId: "1076036960018",
-  appId: "1:1076036960018:web:86c16a257e7e1b699efdcb",
-  measurementId: "G-93GJ8JWRGK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
